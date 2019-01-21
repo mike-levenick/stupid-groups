a76df95358a478b6314b3188937cfcf942649722
Michael Levenick <mlev@new-macbook.local>
2019-01-21 08:39:43 -0600

    Initial Commit

---
+++ Stupid Groups/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Stupid Groups
+//
+//  Created by Michael Levenick on 1/21/19.
+//  Copyright © 2019 Michael Levenick. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Stupid Groups/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stupid Groups
+//
+//  Created by Michael Levenick on 1/21/19.
+//  Copyright © 2019 Michael Levenick. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
